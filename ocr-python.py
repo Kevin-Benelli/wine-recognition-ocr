abf204e34a65d11a858056f79018d1bd3bdcb49f
Kevin Benelli <kevin.e.benelli@gmail.com>
2018-09-20 20:24:34 -0700

    added basic function and incorporated matplotlib to display filtered image(s)

--- ocr-python.py
+++ ocr-python.py
@@ -2,24 +2,43 @@
 
 from PIL import Image, ImageFilter
 import pytesseract
-
+from matplotlib import pyplot as plt
 
 def Basic_Tesseract(image):
-
+    image = image.convert('L')
     return image
 
 
-image_obj_basic = Image.open("UCD_Lehmann_0036.jpg")
+def Zoom_Tesseract(image):
+    image = image.convert('L').resize(
+        [3 * _ for _ in image.size], Image.BICUBIC)
+    image = image.point(lambda p: p > 75 and p + 100)
+    return image
 
+
+# Open image
+image_obj_basic = Image.open("UCD_Lehmann_0036.jpg")
 # converts jpg file extention to tiff
-image_obj_basic.save('UCD_Lehmann_0036.tiff')
+img_tiff = image_obj_basic.save('UCD_Lehmann_0036.tiff')
+# Filter tesseract function
+image_obj_basic = Basic_Tesseract(Image.open("UCD_Lehmann_0036.jpg"))
+
 
 # run tesseract on image object
 text = pytesseract.image_to_string(image_obj_basic, lang='eng')
 
-# Log image to string recognition conversion
+plt.imshow(image_obj_basic)
+plt.show()
+
+# Log image to string recognition conversion for basic
 print(text)
 
 
 # Get verbose data including boxes, confidences, line and page numbers
 print(pytesseract.image_to_data(Image.open('UCD_Lehmann_0036.tiff')))
+
+
+print("#####################################################################")
+print("Zoom Function:")
+
+# image_obj_zoom = Image.open("UCD_Lehmann_0036.jpg")
