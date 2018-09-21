a35890952c7e38a0f2f74fadc3bc7ad404dc2776
Kevin Benelli <kevin.e.benelli@gmail.com>
2018-09-21 09:02:22 -0700

    added feature that display's the filtered image and original- also included file open and close for image filtering in the future

--- ocr-python.py
+++ ocr-python.py
@@ -2,43 +2,75 @@
 
 from PIL import Image, ImageFilter
 import pytesseract
+import cv2
+import numpy as np
 from matplotlib import pyplot as plt
 
+
 def Basic_Tesseract(image):
-    image = image.convert('L')
     return image
 
 
-def Zoom_Tesseract(image):
+def Filter_Tesseract(image):
     image = image.convert('L').resize(
-        [3 * _ for _ in image.size], Image.BICUBIC)
+        [2 * _ for _ in image.size], Image.BICUBIC)
     image = image.point(lambda p: p > 75 and p + 100)
     return image
 
 
-# Open image
-image_obj_basic = Image.open("UCD_Lehmann_0036.jpg")
+# Open jpg image
+image_obj = Image.open("UCD_Lehmann_0036.jpg")
 # converts jpg file extention to tiff
-img_tiff = image_obj_basic.save('UCD_Lehmann_0036.tiff')
-# Filter tesseract function
-image_obj_basic = Basic_Tesseract(Image.open("UCD_Lehmann_0036.jpg"))
+img_tiff = image_obj.save("UCD_Lehmann_0036.tiff")
+# Close jpg image
+image_obj.close()
 
+# Open Tiff image
+image_obj = Image.open("UCD_Lehmann_0036.tiff")
+
+# Basic Filter function
+image_obj_basic = Basic_Tesseract(image_obj)
 
 # run tesseract on image object
 text = pytesseract.image_to_string(image_obj_basic, lang='eng')
 
-plt.imshow(image_obj_basic)
-plt.show()
+print("***************************************************************************************************")
+print("Basic Function:")
+print("***************************************************************************************************")
 
 # Log image to string recognition conversion for basic
 print(text)
 
+# # Get verbose data including boxes, confidences, line and page numbers
+# print(pytesseract.image_to_data(image_obj))
 
-# Get verbose data including boxes, confidences, line and page numbers
-print(pytesseract.image_to_data(Image.open('UCD_Lehmann_0036.tiff')))
+plt.imshow(image_obj_basic)
+
+print("***************************************************************************************************")
+print("Filter Function:")
+print("***************************************************************************************************")
+
+# close edited Tiff image
+image_obj.close()
+# Re-open Tiff image for filtering
+image_obj = Image.open("UCD_Lehmann_0036.tiff")
+
+# Basic Filter function
+image_obj_filter = Filter_Tesseract(image_obj)
+
+# run tesseract on image object
+text = pytesseract.image_to_string(image_obj_filter, lang='eng')
+# log image to text translation
+print(text)
+
+# # Get verbose data including boxes, confidences, line and page numbers
+# print(pytesseract.image_to_data(image_obj))
 
 
-print("#####################################################################")
-print("Zoom Function:")
-
-# image_obj_zoom = Image.open("UCD_Lehmann_0036.jpg")
+# plot image filters
+fig = plt.figure(figsize=(1, 1))
+fig.add_subplot(1, 2, 2)
+plt.imshow(image_obj_filter)
+plt.show(block=False)
+plt.pause(60)
+plt.close()
